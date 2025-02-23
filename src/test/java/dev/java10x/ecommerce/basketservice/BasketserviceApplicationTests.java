dfa772051ca6ab685f69fcc2158ab650adb6b504
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-02-22 23:38:32 -0300

    first commit

---
+++ src/test/java/dev/java10x/ecommerce/basketservice/BasketserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.ecommerce.basketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
