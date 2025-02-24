d475ec1b76ca363b3e546ac917e7727bb469878d
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-02-24 11:23:56 -0300

    feat: adiciona os endpints em ProductController para consumir a API com a lógica no ProductService, junto a configuracao de Caching

--- src/main/java/dev/java10x/ecommerce/basketservice/controller/ProductController.java
+++ src/main/java/dev/java10x/ecommerce/basketservice/controller/ProductController.java
@@ -1,4 +1,35 @@
 package dev.java10x.ecommerce.basketservice.controller;
 
+import dev.java10x.ecommerce.basketservice.client.response.PlatziProductResponse;
+import dev.java10x.ecommerce.basketservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<PlatziProductResponse>> getAllProducts(){
+        List<PlatziProductResponse> allProducts = productService.getAllProducts();
+        return ResponseEntity.ok(allProducts);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlatziProductResponse> getProductById(@PathVariable Long id){
+
+        return ResponseEntity.ok(productService.getProductById(id));
+
+    }
 }
--- src/main/java/dev/java10x/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java10x/ecommerce/basketservice/service/ProductService.java
@@ -1,4 +1,31 @@
 package dev.java10x.ecommerce.basketservice.service;
 
+import dev.java10x.ecommerce.basketservice.client.PlatziStoreClient;
+import dev.java10x.ecommerce.basketservice.client.response.PlatziProductResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final PlatziStoreClient client;
+
+    @Cacheable(value = "products")
+    public List<PlatziProductResponse> getAllProducts(){
+        log.info("Getting all products");
+        return client.getAllProducts();
+    }
+
+    @Cacheable(value = "product", key = "#id")
+    public PlatziProductResponse getProductById(Long id){
+        log.info("Getting product with id: {}", id);
+        return client.getProductById(id);
+    }
+
 }
